f87b97fe3248f1912918fae06945962ff60dbbb5
Jordan Godwin <jordan.godwin13@gmail.com>
2023-10-15 08:29:35 -0500

    Add foundation code for course challenge.

---
+++ challenge.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    //get and verify input
+    
+    //avoid out of range values
+    
+    //process rows
+    
+    puts("Nothing here yet!");
+    return 0;
+}
