6176c213fa2ceec37ddac18cee423870dddcf580
Jordan Godwin <jordan.godwin13@gmail.com>
2023-10-15 17:53:21 -0500

    Fix seg fault error.

--- types.c
+++ types.c
@@ -9,13 +9,13 @@ int main() {
     /*input*/
 
     printf("Type a character: ");
-    scanf("%c", c);
+    scanf("%c", &c);
     printf("Type an integer: ");
-    scanf("%d", i);
+    scanf("%d", &i);
     printf("Type a small value: ");
-    scanf("%f", f);
+    scanf("%f", &f);
     printf("Type a huge value: ");
-    scanf("%lg", d);
+    scanf("%lg", &d);
 
     printf("\nCharacter: %c\n", c);
     printf("Integer: %d\n", i);
