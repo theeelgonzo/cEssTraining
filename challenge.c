669f1e79eaf5938fe3490b250d2971697f773791
Jordan Godwin <jordan.godwin13@gmail.com>
2023-10-27 15:37:49 -0500

    Solve chapter challenge.

--- challenge.c
+++ challenge.c
@@ -2,13 +2,22 @@
 #include <stdlib.h>
 
 int main(){
+
+    int rows;
+    int a;
     
     //get and verify input
-    
+    printf("How many rows (18 max)? ");
+    scanf("%d", &rows);
     //avoid out of range values
+    if(rows>18)
+        rows = 18;
     
     //process rows
-    
-    puts("Nothing here yet!");
+    printf("I will process %d rows\n", rows);
+    a = rows >> 1;
+    printf("%d is half of %d\n", a, rows);
+    a = rows << 1;
+    printf("%d is double %d", a, rows);
     return 0;
 }
